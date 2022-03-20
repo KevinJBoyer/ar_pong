8e2b9f4207741af9b315ad2ed814b684ea5d04f9
Kevin Boyer <kevinboyer@navapbc.com>
2022-03-20 17:04:18 -0400

    move class variables in HandsTracker to instance variables

--- handstracker.py
+++ handstracker.py
@@ -7,34 +7,32 @@ from objecttracker.point import Point
 
 
 class HandsTracker:
-    # the specific landmark in the model hand to center the hand
-    tracking_landmark = mp.solutions.hands.HandLandmark.MIDDLE_FINGER_MCP
+    def __init__(
+        self,
+        model_complexity=1,
+        min_detection_confidence=0.1,
+        min_tracking_confidence=0.8,
+    ):
+        # the specific landmark in the model hand to center the hand
+        self.tracking_landmark = mp.solutions.hands.HandLandmark.MIDDLE_FINGER_MCP
 
-    # accuracy vs. latency settings for mp.solutions.hands.Hands
-    model_complexity = 1
-    min_detection_confidence = 0.1
-    min_tracking_confidence = 0.8
-
-    tracked_hands: MultiObjectTracker
-    detected_hands: Optional[Any] = None
-
-    def __init__(self):
         self.tracked_hands = MultiObjectTracker(num_objects=2)
 
+        self.mp_detected_hands: Optional[Any] = None
         self.mp_hands = mp.solutions.hands.Hands(
             static_image_mode=False,
-            model_complexity=self.model_complexity,
-            min_detection_confidence=self.min_detection_confidence,
-            min_tracking_confidence=self.min_tracking_confidence,
+            model_complexity=model_complexity,
+            min_detection_confidence=min_detection_confidence,
+            min_tracking_confidence=min_tracking_confidence,
         )
 
     def update(self, img) -> None:
-        self.detected_hands = self.mp_hands.process(img).multi_hand_landmarks
+        self.mp_detected_hands = self.mp_hands.process(img).multi_hand_landmarks
 
         coords: list[Point] = []
 
-        if self.detected_hands is not None:
-            for hand_coords in self.detected_hands:
+        if self.mp_detected_hands is not None:
+            for hand_coords in self.mp_detected_hands:
                 x = hand_coords.landmark[self.tracking_landmark].x
                 y = hand_coords.landmark[self.tracking_landmark].y
                 coords.append(Point(x, y))
@@ -43,8 +41,8 @@ class HandsTracker:
         self.tracked_hands.update_current_locations()
 
     def draw(self, img) -> None:
-        if self.detected_hands is not None:
-            for hand_landmarks in self.detected_hands:
+        if self.mp_detected_hands is not None:
+            for hand_landmarks in self.mp_detected_hands:
                 mp.solutions.drawing_utils.draw_landmarks(
                     img,
                     hand_landmarks,
