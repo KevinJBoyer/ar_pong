2759b1015cb738d9d0be62a81d207be8ff11937b
Kevin Boyer <kevinboyer@navapbc.com>
2022-03-20 17:18:10 -0400

    add pyglet to project

--- main.py
+++ main.py
@@ -3,40 +3,36 @@ import time
 
 from handstracker import HandsTracker
 
-cap = cv2.VideoCapture(0)
 
-pTime = 0.0
-cTime = 0.0
+def main():
+    camera_id = 0
+    camera = cv2.VideoCapture(camera_id)
 
-RUN = True
-success, img = cap.read()
-h, w, c = img.shape
-hands_tracker = HandsTracker()
+    RUN = True
+    success, img = camera.read()
+    h, w, c = img.shape
+    hands_tracker = HandsTracker()
 
-while RUN:
-    success, img = cap.read()
-    img = cv2.flip(img, 1)
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    while RUN:
+        success, img = camera.read()
+        img = cv2.flip(img, 1)
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    hands_tracker.update(imgRGB)
-    hands_tracker.draw(img)
+        hands_tracker.update(imgRGB)
+        hands_tracker.draw(img)
 
-    for hand in hands_tracker.tracked_hands.object_trackers:
-        if hand.current_location is not None:
-            x = hand.current_location.x
-            y = hand.current_location.y
-            cv2.circle(img, (int(w * x), int(h * y)), 25, (0, 255, 255), cv2.FILLED)
+        for hand in hands_tracker.tracked_hands.object_trackers:
+            if hand.current_location is not None:
+                x = hand.current_location.x
+                y = hand.current_location.y
+                cv2.circle(img, (int(w * x), int(h * y)), 25, (0, 255, 255), cv2.FILLED)
 
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
+        cv2.imshow("Image", img)
 
-    cv2.putText(
-        img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3
-    )
+        keypress = cv2.waitKey(1)
+        if keypress == 27:  # Escape key
+            RUN = False
 
-    cv2.imshow("Image", img)
 
-    keypress = cv2.waitKey(1)
-    if keypress == 27:  # Escape key
-        RUN = False
+if __name__ == "__main__":
+    main()
